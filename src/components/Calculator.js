5727fa3556b269dd9f879a3f3864ee3717febe1e
Mong'are <Mongaresams@gmail.com>
2022-01-06 15:43:23 +0300

    Fix linter errors and rename function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,10 +6,10 @@ export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.handleClick = this.handleClick.bind(this);
+    this.clickMe = this.clickMe.bind(this);
   }
 
-  handleClick(e) {
+  clickMe(e) {
     this.setState((state) => calculate(state, e.target.textContent));
   }
 
@@ -20,25 +20,25 @@ export default class Calculator extends Component {
         <div className="output">
           {next || total || 0}
         </div>
-        <button type="button" onClick={this.handleClick}>AC</button>
-        <button type="button" onClick={this.handleClick}>+/-</button>
-        <button type="button" onClick={this.handleClick}>%</button>
-        <button type="button" onClick={this.handleClick} className="math-sign">÷</button>
-        <button type="button" onClick={this.handleClick}>7</button>
-        <button type="button" onClick={this.handleClick}>8</button>
-        <button type="button" onClick={this.handleClick}>9</button>
-        <button type="button" onClick={this.handleClick} className="math-sign">x</button>
-        <button type="button" onClick={this.handleClick}>4</button>
-        <button type="button" onClick={this.handleClick}>5</button>
-        <button type="button" onClick={this.handleClick}>6</button>
-        <button type="button" onClick={this.handleClick} className="math-sign">-</button>
-        <button type="button" onClick={this.handleClick}>1</button>
-        <button type="button" onClick={this.handleClick}>2</button>
-        <button type="button" onClick={this.handleClick}>3</button>
-        <button type="button" onClick={this.handleClick} className="math-sign">+</button>
-        <button type="button" onClick={this.handleClick} className="span-two">0</button>
-        <button type="button" onClick={this.handleClick}>.</button>
-        <button type="button" onClick={this.handleClick} className="math-sign">= </button>
+        <button type="button" onClick={this.clickMe}>AC</button>
+        <button type="button" onClick={this.clickMe}>+/-</button>
+        <button type="button" onClick={this.clickMe}>%</button>
+        <button type="button" onClick={this.clickMe} className="math-sign">÷</button>
+        <button type="button" onClick={this.clickMe}>7</button>
+        <button type="button" onClick={this.clickMe}>8</button>
+        <button type="button" onClick={this.clickMe}>9</button>
+        <button type="button" onClick={this.clickMe} className="math-sign">x</button>
+        <button type="button" onClick={this.clickMe}>4</button>
+        <button type="button" onClick={this.clickMe}>5</button>
+        <button type="button" onClick={this.clickMe}>6</button>
+        <button type="button" onClick={this.clickMe} className="math-sign">-</button>
+        <button type="button" onClick={this.clickMe}>1</button>
+        <button type="button" onClick={this.clickMe}>2</button>
+        <button type="button" onClick={this.clickMe}>3</button>
+        <button type="button" onClick={this.clickMe} className="math-sign">+</button>
+        <button type="button" onClick={this.clickMe} className="span-two">0</button>
+        <button type="button" onClick={this.clickMe}>.</button>
+        <button type="button" onClick={this.clickMe} className="math-sign">= </button>
       </div>
     );
   }
