f118d30fae8adfce13d642cb3067bc7e11bbecc7
Mong'are <Mongaresams@gmail.com>
2022-01-14 06:24:46 +0300

    Update page data of quotes page

--- src/components/pages/Quotes.js
+++ src/components/pages/Quotes.js
@@ -1,7 +1,26 @@
-const Quotes = () => (
-  <div>
-    <h1>I want you to ruin my nights</h1>
-  </div>
-);
+/* eslint-disable */
+import React, { Component } from 'react'
 
-export default Quotes;
+export default class Quotes extends Component {
+    state = {
+      quotes: [],
+    };
+    
+     componentDidMount() {
+    const apiUrl = 'https://random-math-quote-api.herokuapp.com/';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => this.setState({ quotes: data }));
+  }
+  
+    render() {
+        return (
+        <div className="quote__content">
+      <p>{this.state.quotes.quote}</p>
+      <p>{this.state.quotes.author}</p>
+    </div>
+        )
+    }
+}
+
+
